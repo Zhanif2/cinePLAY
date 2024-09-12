bf343c5be2f5f9937773d0431cffc466905fd1c5
z h <zhanif02@gmail.com>
2024-09-12 15:03:30 +0100

    css for lp

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,8 +3,17 @@ import lp from '../assets/homepage-img.jpg'
 
 const Landing = () => {
   return (
-    <div>
-    <img src={lp} alt="" className='landing__page--img' />
+    <div className="container">
+      <div className="column">
+        <div className="landing__page--contents">
+          <h1>Welcome to <span className="glow__effect">CinePLAY</span></h1>
+          <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, corrupti? Libero corporis deserunt natus, quidem consequatur repellat. Quibusdam, ex quos.</p>
+          <button className='btn'>explore now</button>
+        </div>
+        <div className="img">
+          <img src={lp} alt="" className='landing__page--img'/>
+        </div>
+      </div>
     </div>
   )
 }
